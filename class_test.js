38e3b3188e4f62f3d8961347dc77d1a9de037083
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-22 13:40:42 +0600

    Create class_test.js

---
+++ class_test.js
@@ -0,0 +1,215 @@
+// number 1
+const node = document.getElementById('node');
+//number 2
+//first write the keyword(var,let, const)
+//variable name
+// equal to sign
+//valuie
+
+//number 3
+
+//string
+const a = "bangladesh";
+//integer
+const b = 20;
+//float
+const c = 3.4;
+//object
+const d = {'id':1,'name':'rayhan'};
+
+//number 4
+// you have to follow certain rule like camel casing.
+//don't use keywords as variable name.
+//use underscores or camel casing or other variable naming rule if there is more than one word
+//name has to be relating to the variable objective
+
+//number 5
+const name = "Rayhan";
+
+//number 6
+// arithmetic operators are used for mathmetical operations
+const sum = 4 +4;
+
+//number 7
+
+// %modules is used to find out remaining of division
+const result = 8%3;
+
+// ** this sign is used for showing power of number for example
+const num= num**3;
+// ++ this sign is used for incremement. if you want to run a loop and do certain increment you can use this sign
+const number =++ number;
+//-- this sign is used for derement.
+// / this sign is used for division.
+
+//number 8
+// the variable that operators work on is operand.
+
+//number 9
+const x=10;
+const y=36;
+const z= x+y;
+console.log(z)
+
+// number 10
+const remaining = 375%8;
+console.log(remaining);
+
+//number 11
+const power=8**3;
+console.log(power)
+
+//number 12
+const math = 10;
+const physics= 20;
+const chemistry= 40;
+const bangla= 60;
+
+const res = (math+physics+chemistry+bangla)/4;
+
+//number 13
+var xx = 30;
+var yy='23';
+var zz = xx+yy;
+//result woudl be 3023
+
+
+//number 14
+
+//typeof is used to know the type of an object/variable 
+const typecheking= 34
+console.log(typeof(typecheking))
+
+//number 15
+const conversion = '23';
+const converting = parseInt(conversion);
+
+//number 16
+// Arrays index starts from 0
+
+// number 17
+var numbers=[34,56,74,24,88,34,776,34,5];
+numbers.splice(5,1,334)
+console.log(numbers)
+
+//number 18
+
+numbers.push(234)
+numbers.unshift(998)
+
+//number 19
+var numbersArray =[[1,2],[3,4],[5,6]]
+console.log(numbersArray.flat())
+
+// number 20
+var team1 = ['rahim','karim'];
+var team2 = ['tanim','biplob','tarek'];
+var team3 = ['sajjad','arefin']
+var teamComb = team1.concat(team2,team3)
+
+// number 21
+var fruits = ['Banana','Orange','Apple','Mango']
+fruits.shift()
+
+//number 22
+fruits.splice(3,0,'Lichi','Pineapple','watermelon')
+console.log(fruits)
+
+//number 23
+const num1 = 354
+const num2 = 454
+if(num1>num2){
+    console.log("number 1 is bigger",num1)
+}
+else{
+    console.log("number 2 is bigger",num2)
+}
+
+//24 number
+let number1;
+let number2;
+let operator;
+
+if( operator== '+'){
+    number1+number2
+}
+else if(operator == '-'){
+    number1-number2
+}
+else if(operator == '/'){
+    number1/number2 
+}
+else if (operator=='*'){
+    number1*number2
+}
+else{
+    console.log("invaild operator")
+}
+
+
+
+//25 number
+
+const pen = 185/10*3;
+console.log(pen)
+// 26 number
+const numb2=60
+const rest = (1/100)*numb2
+console.log(rest)
+//27 number
+if(fruits.indexOf('kiwi')== -1){
+    console.log("This Fruit does not exists on the array")
+}
+else{
+    console.log("This fruist exists on the array")
+}
+
+//28 number
+const singara=20
+const somucha=15
+const bon = 10
+const burder= 60
+const amountYouHave = 34
+if(amountYouHave<10){
+    console.log("you can have nothing")
+}
+else if(amountYouHave<20){
+    console.log("you can have bon or somucha")
+}
+else if(amountYouHave<50){
+    console.log("you can have bon or somucha or singara or all of them")
+}
+else if(amountYouHave>60){
+    console.log("you can have anything")
+}
+//number 29
+const marks=34
+if(marks>90){
+    console.log("S grade")
+}
+else if(marks>80){
+    console.log("A grade")
+}
+
+else if(marks>70){
+    console.log("B grade")
+}
+
+else if(marks>60){
+    console.log("C grade")
+}
+
+else if(marks>50){
+    console.log("D grade")
+}
+else if(marks>40){
+    console.log("E grade")
+}
+else{
+    console.log("Student has failed")
+}
+
+//number 30
+//time is the most important assets we have. so it's important to use our time cautiously. i can use my time for my study and programming so that i can build my future strongly.
+//I think i should use most of my time to study about new technology that has been evolving by the time to be best in my field. I'm not using my time these days in best way because of 
+//personal problem. but i hope to come back strongly and be the best.
